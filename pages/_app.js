fc5f40329b56b065a8ff63603848ab4053920d8b
wilyyy <alvarez_william@outlook.com>
2022-01-07 14:03:25 -0800

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <title>Vigilant</title>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta 
+          name="Vigilant" 
+          content="2 Week javascript referesher for Web Dev 4 class at BCIT D3. Created by William Alvarez,
+          Josh Reyes, Aryan Heravi & Maggie Su" 
+      />
+    </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
