05b9cd2eeef5549138a176ada93be42a86043cd5
maggiesuu <msu28@my.bcit.ca>
2022-01-09 12:50:23 -0800

    index.js changes

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import Header from '../comps/Header';
-
 export default function Home() {
   return (
     <>
-    <Header></Header>
       stuff
     </>
   )
