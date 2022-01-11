1ceda56ab5be23fc747d092eb2d26cec13da1e38
joshbreyes <jreyes46@my.bcit.ca>
2022-01-10 20:14:18 -0800

    finished report button and start button

--- comps/Button.js
+++ comps/Button.js
@@ -1,16 +1,28 @@
 import styled from 'styled-components';
 
 const Container = styled.button`
-    background-color: #000;
-    color: #fff;
+    background-color: #C4C4C4;
+    color: #000;
     display: flex;
     justify-content: center;
     align-items: center;
+    height: 40px;
+    border-radius: 8px;
+    border-style: none;
+    width: 180px;
+`;
+const ButtonText = styled.text`
+    font-size: 28px;
+    font-weight: bold;
 `;
 
 const Button = () => {
     return (
-        <Container>Button</Container>
+        <Container>
+            <ButtonText>
+                Start
+            </ButtonText>
+        </Container>
     )
 }
 
