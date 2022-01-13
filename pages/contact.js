4486ed60209b1964b398621bb31308b26492f64d
wilyyy <alvarez_william@outlook.com>
2022-01-12 16:35:54 -0800

    test deploy

--- pages/contact.js
+++ pages/contact.js
@@ -5,31 +5,31 @@ import Header from '../comps/Header'
 
 export default function Contact() {
     return(
-        <><Header></Header>
-        <div className={styles.container}>
+        <>
+            <Header></Header>
+            <div className={styles.container}>
 
-            <form className={styles.main}>
-                <h1>Contact Form</h1>
+                <form className={styles.main}>
+                    <h1>Contact Form</h1>
 
-                <formGroup className={styles.inputGroup}>
-                    <label htmlFor='name'>Name</label>
-                    <input type='text' name='name' className={styles.inputField} />
-                </formGroup>
+                    <formGroup className={styles.inputGroup}>
+                        <label htmlFor='name'>Name</label>
+                        <input type='text' name='name' className={styles.inputField} />
+                    </formGroup>
 
-                <formGroup className={styles.inputGroup}>
-                    <label htmlFor='email'>Email</label>
-                    <input type='email' name='email' className={styles.inputField} />
-                </formGroup>
+                    <formGroup className={styles.inputGroup}>
+                        <label htmlFor='email'>Email</label>
+                        <input type='email' name='email' className={styles.inputField} />
+                    </formGroup>
 
-                <formGroup className={styles.inputGroup}>
-                    <label htmlFor='message'>Message</label>
-                    <input type='text' name='message' className={styles.inputField1} />
-                </formGroup>
+                    <formGroup className={styles.inputGroup}>
+                        <label htmlFor='message'>Message</label>
+                        <input type='text' name='message' className={styles.inputField1} />
+                    </formGroup>
 
-                < input type='submit'/>
-
-                
-            </form>
-        </div></>
+                    < input type='submit'/>
+                </form>
+            </div>
+        </>
     )
 }
